8ea261049cfaf5b410b060d485442ab66a911c88
Pascal Burkhard <pascal.burkhard@gmail.com>
2021-06-04 15:23:18 +0200

    Corrections

--- py/projet2/exercice3_exemple.py
+++ py/projet2/exercice3_exemple.py
@@ -9,7 +9,7 @@ for adresse in adresses_dict:
   adresse["Tel"] = re.sub(r"(.*)", r"+41 \1 \2 \3 \4", adresse["Tel"])
   
 keys = adresses_dict[0].keys()
-with open(folder_path + 'adresses_clean.csv', 'w', newline='')  as output_file:
+with open(folder_path + 'adresses_clean.csv', 'w', newline='') as output_file:
     dict_writer = csv.DictWriter(output_file, keys)
     dict_writer.writeheader()
     dict_writer.writerows(adresses_dict)
